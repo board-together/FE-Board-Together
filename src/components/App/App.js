3e033b73db456c3a939ec74461757fe0653a3c7d
Spencer Haka <spencer.haka@gmail.com>
2023-02-05 13:25:22 -0700

    Add routes in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import User_Dashboard from '../User_Dashboard/User_Dashboard'
+import Login from '../Login/Login'
+import Search_Results from '../Search_Results/Search_Results'
+import Friends_Games from '../Friends_Games/Friends_Games'
 
 export const App = () => {
+
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<User_Dashboard />}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/search-result/:searchTerm' element={<Search_Results />}/>
+      <Route path='/friends-games/:id' element={<Friends_Games />}/>
+    </Routes>
   )
 }
