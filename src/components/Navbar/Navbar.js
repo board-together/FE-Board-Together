a55ded9d19601bb7f2c19eaf7407e2d75a9a0e7a
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-02-05 15:49:40 -0700

    Adds searchbar component and basic nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import Searchbar from '../Searchbar/Searchbar'
+import './Navbar.css'
+
+
+
+
+
+
+
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'> 
+      <h2 className='welcome-greeting'>Welcome User</h2>
+      <h1 className='app-name'>Board Together</h1>
+     <Searchbar></Searchbar>
+    </div>
   )
 }
---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,30 @@
+import './Searchbar.css'
+import React, { useState } from 'react';
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // code to fetch board games matching the search term and update the results state
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder='Look for a game!' value={searchTerm} onChange={handleChange} />
+      <button type="submit">Search</button>
+      <ul>
+        {results.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </form>
+  );
+};
+
+export default SearchBar;
