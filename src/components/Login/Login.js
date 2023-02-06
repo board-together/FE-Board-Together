182749ddfb8237bb62976d38b62fbbe8fedef9fb
Brett Kuhn <brettkuhn87@gmail.com>
2023-02-05 20:10:24 -0700

    Added basic JSX to Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <main>
+      <div>
+        <h1>Welcome to Board Together</h1>
+        <p>Login</p>
+      </div>
+      <div>
+        <input />
+        <Link to='/'>
+            <button>Enter</button>
+        </Link>
+        <p>Not a member?</p>
+      </div>
+    </main>
   )
 }
