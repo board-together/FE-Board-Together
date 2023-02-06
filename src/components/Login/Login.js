a4fe5ac4177850d8e1c71216f86c732b4ed9a1c7
Brett Kuhn <brettkuhn87@gmail.com>
2023-02-05 21:57:25 -0700

    Adding conditional rendering and functionality to check if username is already existing

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../Login/Login.css'
 
 export const Login = () => {
 
-  const [userNameInput, setUserNameInput] = useState('')
+  /* Need a prop function to set 'username' state in App? */
+
+  const [userNameInput, setUserNameInput] = useState('');
+  const [validUser, setValidUser] = useState(false);
+  const [userStatus, setUserStatus] = useState(true);
+  const [newUserInput, setNewUserInput] = useState('');
+  const [invalidUserName, setInvalidUserName] = useState(false)
+  const [existingUserNames, setExistingUserNames] = useState([]);
+
+  useEffect(() => {
+    //when page loads, populate existingUserNames with fetch/graphquery etc
+    setExistingUserNames(['test1', 'test2'])
+  }, []);
+
+  useEffect(() => {
+    existingUserNames.includes(userNameInput) ? setValidUser(true) : setValidUser(false);
+  }, [userNameInput]);
+
+  const handleNewUserClick = (event) => {
+    event.preventDefault();
+    setUserStatus(false);
+  } 
+
+  const addName = (name) => {
+    setUserStatus(true);
+    setExistingUserNames([...existingUserNames, name]); 
+    setInvalidUserName(false);
+    //will need to change to make this a post fetch etc
+  }
+
+  const submitNewUser = (event) => {
+    event.preventDefault()
+    existingUserNames.includes(newUserInput) ? setInvalidUserName(true) : addName(newUserInput)
+  }
 
   return (
     <main>
@@ -12,20 +45,49 @@ export const Login = () => {
         <h1 className='login-welcome-text'>Welcome to Board Together</h1>
         <h2 className='login-text'>Login</h2>
       </div>
-      <form className='login-form'>
-        <input
-          className='username-input'
-          type='text'
-          placeholder='Enter your user name'
-          value={userNameInput}
-          onChange={event => setUserNameInput(event.target.value)}
-        />
-        <Link to='/'>
-            <button className='enter-site-button'>Enter</button>
-        </Link>
-        <p>Not a member?</p>
-        <button className='create-account-button' type='button'>Create an account</button>
-      </form>
+      {userStatus && <>
+        <form className='login-form'>
+          <input
+            className='username-input'
+            type='text'
+            placeholder='Enter your username'
+            value={userNameInput}
+            onChange={event => setUserNameInput(event.target.value)}
+          />
+          {validUser && <Link to='/'>
+              <button className='enter-site-button'>Enter</button>
+          </Link>}
+        </form>
+        <div className='not-member-prompt-area'>
+          <p className='not-member-prompt'>Not a member?</p>
+          <button 
+            className='create-account-button' 
+            type='button'
+            onClick={event => handleNewUserClick(event)}
+          >
+            Create an account
+          </button>
+        </div>      
+        </>
+      }
+      {!userStatus &&
+        <form className='create-user-form'>
+          <input
+            className='new-user-input'
+            type='text'
+            placeholder='Type the username you want to have'
+            value={newUserInput}
+            onChange={event => setNewUserInput(event.target.value)}
+          />
+          <button
+            className='create-new-user-button'
+            onClick={event => submitNewUser(event)}
+          >
+            Create
+          </button>
+          {invalidUserName && <p>Username already taken!</p>}
+        </form>
+      }
     </main>
   )
 }
