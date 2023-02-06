1e330667367b380ba41df8c9b4991610c13650b9
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-02-06 16:43:51 -0700

    Features of the navbar are now tested with cypress

---
+++ cypress/e2e/navbar.spec.cy.js
@@ -0,0 +1,23 @@
+describe('Navbar', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+    cy.get('.navbar-container')
+  })
+  
+  it('Should have a navbar with a site title', () => {
+    cy.get('.app-name').contains('Board Together')
+  })
+
+  it('Should have a navbar with a welcome greeting', () => {
+    cy.get('.welcome-greeting').contains('Welcome,')
+  })
+
+  it('Should have a navbar with a searchbar that can search and redirect to the corect url', () => {
+    cy.get('.search-input')
+      .type('Catan');
+    cy.get('.search-button')
+      .click();
+    cy.url()
+      .should('include', 'http://localhost:3000/search-results/Catan');
+  })
+})
