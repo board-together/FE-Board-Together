5b37aac8e0cf9ef97ab214ded992cf31b9f052d7
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-02-09 15:05:45 -0700

    Adds Navbar to the search results page

--- src/components/Search_Results/Search_Results.js
+++ src/components/Search_Results/Search_Results.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Navbar } from '../Navbar/Navbar'
 import SingleGame from '../Single_Game/Single_Game'
 
-const SearchResults = ({ results }) => {
+const SearchResults = ({ results, searchBarSubmit }) => {
   const games = results.map(game => <SingleGame game={game} />)
 
   return (
+  <div>
+    <Navbar searchBarSubmit={searchBarSubmit} ></Navbar>
     <div className='game-tiles'>
       {!games.length && <h1>No results matching that name were found.</h1>}
       {games}
     </div>
+  </div>
   )
 }
 
