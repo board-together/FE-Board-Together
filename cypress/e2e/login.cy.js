e113f6907cbd4c7d0224a2dd54bea6018001dd8f
Brett Kuhn <brettkuhn87@gmail.com>
2023-02-09 18:12:41 -0700

    Adds remaining tests for Login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -11,6 +11,25 @@ describe('Login Page', () => {
   });
 
   it('Should have an input where user can enter their username', () => {
+    cy.get('.username-input').should('have.value', '')
+      .type('randy')
+      .should('have.value', 'randy');
+  });
+
+  it('Should only let the user into the site if they enter a valid username', () => {
+    cy.get('.username-input')
+      .type('barfyman342');
+    cy.get('.invalid-user-button')
+      .click();
+    cy.get('.invalid-name-message').should('have.text', 'Please enter a valid username')
+  });
+
+  it('Should take the user to their dashboard when they enter their username', () => {
+    cy.get('.username-input')
+      .type('randy');
+    cy.get('.enter-site-button')
+      .click()
+      .url().should('equal', 'http://localhost:3000/dashboard/randy')
 
     
 
