60357a5f0d526726df719adead1f1947afbc9fde
Brett Kuhn <brettkuhn87@gmail.com>
2023-02-09 20:04:21 -0700

    Adds conditional rendering for loading, error, and data, depending on success of cql query

--- src/components/User_Dashboard/User_Dashboard.js
+++ src/components/User_Dashboard/User_Dashboard.js
@@ -12,6 +12,8 @@ export const UserDashboard = ({ userInfo, searchBarSubmit }) => {
   const userName = useParams().username;
   const { loading, error, data } = useQuery(GET_USER(userName));
   console.log('Data from GraphQL Query: ', data);
+  console.log(`'Loading': `, loading);
+  console.log(`'Error: `, error);
 
   let borrowedGamesThumbnails = fakeBorrowedGames.games.map((game, index) => <SingleGame key={index} game={game}/>)
   let friends = userInfo.friends.map(friend => <p key={friend} className="friend">{friend}</p>)
@@ -21,17 +23,21 @@ export const UserDashboard = ({ userInfo, searchBarSubmit }) => {
     <>
       <Navbar username={userInfo.username} searchBarSubmit={searchBarSubmit}/>
       <div className='user-dashboard'>
-        <div className='game-collection-section'>
-          <h1 className='my-games-heading'>My Games</h1>
-          <h2>Games I'm Borrowing</h2>
-          <div className='borrowed-games-collection'>{borrowedGamesThumbnails}</div>
-          <h2>My Game Collection</h2>
-          <div className='game-collection'>{games}</div>
-        </div>
-        <div className='friends-section'>
-          <h1 className='my-friends-header'>My Friends</h1>
-          <div className='friends-list'>{friends}</div>
-        </div>
+        {error && <h1>Error loading data: {error.message}</h1>}
+        {loading && <h1>Loading...</h1>}
+        {data && <>
+          <div className='game-collection-section'>
+            <h1 className='my-games-heading'>My Games</h1>
+            <h2>Games I'm Borrowing</h2>
+            <div className='borrowed-games-collection'>{borrowedGamesThumbnails}</div>
+            <h2>My Game Collection</h2>
+            <div className='game-collection'>{games}</div>
+          </div>
+          <div className='friends-section'>
+            <h1 className='my-friends-header'>My Friends</h1>
+            <div className='friends-list'>{friends}</div>
+          </div>
+        </>}
       </div>
     </>
   )
