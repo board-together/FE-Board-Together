cb5170ddd410a8f31125e2418f52523ebb17183e
Spencer Haka <spencer.haka@gmail.com>
2023-02-09 22:26:13 -0700

    Start Modal Tests

--- cypress/e2e/modal.cy.js
+++ cypress/e2e/modal.cy.js
@@ -1,3 +1,11 @@
 describe('Modal', () => {
-  
+
+  beforeEach(() => {
+    cy.visit(`http://localhost:3000/`)
+  });
+
+  it("should open the modal when a game is clicked on", () => {
+    cy.get('.single-tile').first().click()
+    cy.get('.modal-container').should('be.visible')
+  })
 })
--- src/components/Search_Results/Search_Results.js
+++ src/components/Search_Results/Search_Results.js
@@ -6,7 +6,6 @@ import SingleGame from '../Single_Game/Single_Game'
 const SearchResults = ({ results, searchBarSubmit, userInfo }) => {
   const games = results.map(game => <SingleGame game={game} key={game.id}/>)
  
-
   return (
   <div>
       <Navbar searchBarSubmit={searchBarSubmit} ></Navbar>
