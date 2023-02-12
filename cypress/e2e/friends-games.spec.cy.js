9e719b82095aabbae116f589f8110fe288c34a18
Brett Kuhn <brettkuhn87@gmail.com>
2023-02-12 16:31:06 -0700

    Updates test spec for friends games to set a user then visit page beforeEach, and also update tests to grab updated classe

--- cypress/e2e/friends-games.spec.cy.js
+++ cypress/e2e/friends-games.spec.cy.js
@@ -1,15 +1,19 @@
 describe('Friends_Games', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/friends-games/Larry')
-
+    cy.visit('http://localhost:3000/')
+    cy.get('.username-input')
+      .type('randy');
+    cy.get('.enter-site-button')
+      .click()
+    cy.get('.friend').eq(0).click()
   })
 
   it('Should have a list of games ', () => {
-    cy.get('#game-name-1').contains("Dominion")  
+    cy.get('.single-game-name').eq(0).should('have.text', 'Ticket to Ride')  
   })
 
   it('Should have a name showing whos games the user is viewing', () => {
-    cy.get('.friend-name').contains('Larrys Games')
+    cy.get('.friend-name').contains(`Pickafloof's Games`)
   })
 
   it('Should have a button that takes the useer back to there dashboard', () => {
