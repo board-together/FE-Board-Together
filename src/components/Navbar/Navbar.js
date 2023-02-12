aaa8d506beadb084d19e4f9804013be46f4cf558
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-02-12 14:25:28 -0700

    Deletes comments on navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,7 +13,6 @@ export const Navbar = ({ searchBarSubmit, username }) => {
   return (
     <div className='navbar-container'>
       <h2 className='welcome-greeting'>Welcome, {formatedUsername}!</h2>
-      {/* make the above not a string when we take in a user.  */}
       <h1 className='app-name'>Board Together</h1>
       <Searchbar submit={searchBarSubmit} />
     </div>
