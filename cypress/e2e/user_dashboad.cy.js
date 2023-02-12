d319cccc1cc71adf5c38e63219e5af5cc199f671
Brett Kuhn <brettkuhn87@gmail.com>
2023-02-12 15:24:37 -0700

    Updates user dashboard tests to account for new Randy data

--- cypress/e2e/user_dashboad.cy.js
+++ cypress/e2e/user_dashboad.cy.js
@@ -26,27 +26,23 @@ describe('User Dashboard', () => {
   it('should show the user\'s list of friends', () => {
     cy.get('.friends-section').contains('My Friends')
     cy.get('.friends-list').should('be.visible')
-    cy.get('.friend').first().contains('Larry')
-    cy.get('.friend').eq(1).contains('Terry')
-    cy.get('.friend').eq(2).contains('Jerry')
-    cy.get('.friend').last().contains('Harry')
+    cy.get('.friend').first().contains('Pickafloof')
+    cy.get('.friend').eq(1).contains('mikedao')
+    cy.get('.friend').eq(2).contains('abdulredd')
+    //Will need to test all or stub data
   })
 
   it('should have a persistent Navbar', () => {
   })
 
   it('should show the user\'s games', () => {
-    cy.contains('Dominion')
-    cy.contains('Carcassonne')
-    cy.contains('Scythe')
+    cy.contains('Ticket to Ride')
+    cy.contains('Rummikub')
     cy.get('.game-collection')
       .find('.single-game-img')
-      .eq(0).should('have.attr', 'src', "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254200326-6135RVKbZZL.jpg")
+      .eq(0).should('have.attr', 'src', "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324738308.jpg")
     cy.get('.game-collection')
       .find('.single-game-img')
-      .eq(1).should('have.attr', 'src', "https://cdn11.bigcommerce.com/s-wue8xznink/images/stencil/1280x1280/products/357/17642/pic6544250_1__09565.1658423124.png?c=2")
-      cy.get('.game-collection')
-      .find('.single-game-img')
-      .eq(2).should('have.attr', 'src', "https://cdn.shopify.com/s/files/1/0513/4077/1515/products/scythe-board-game.jpg?v=1611090922")
+      .eq(1).should('have.attr', 'src', "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254849801-61DrzE6DaPL.jpg")
   })
 })
