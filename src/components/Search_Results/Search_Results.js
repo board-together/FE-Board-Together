ae1934c02ab0825edef235c12236adc8c91af667
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-02-13 12:32:23 -0700

    Updates with conditional rendering

--- src/components/Search_Results/Search_Results.js
+++ src/components/Search_Results/Search_Results.js
@@ -2,18 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Navbar } from '../Navbar/Navbar'
 import SingleGame from '../Single_Game/Single_Game'
+import { useQuery } from "@apollo/client"
+import { GET_SEARCHED_GAMES } from '../../GraphQL/queries'
 
 const SearchResults = ({ results, searchBarSubmit, userInfo, setModal }) => {
-  console.log(results);
-  const games = results.map(game => <SingleGame game={game} key={game.id} setModal={setModal}/>)
+  const { loading, data, error } = useQuery(GET_SEARCHED_GAMES(results));
+  console.log(data, error)
 
+
+  if (loading) {
+    return <h1>loading...</h1>
+  }
+  const games = data ? data.searchGames.map(game => <SingleGame game={game} key={game.name} setModal={setModal} />) : []
   return (
     <div>
       <Navbar searchBarSubmit={searchBarSubmit} ></Navbar>
-      <Link to={`/dashboard/${userInfo.username}`}><button>Back to dashboard</button></Link>
+      <Link to={`/dashboard/`}><button>Back to dashboard</button></Link>
       <div className='game-tiles'>
-        {!games.length && <h1>No results matching that name were found.</h1>}
-        {games}
+        {!games.length ? <h1>No results matching that name were found.</h1> : games}
       </div>
     </div>
   )
