53b3a503df014918f07a12c67e832e4b77f5880a
Brett Kuhn <brettkuhn87@gmail.com>
2023-02-13 11:07:36 -0700

    Adds query to show who is borrowing yuor game

--- src/components/Single_Game/Single_Game.js
+++ src/components/Single_Game/Single_Game.js
@@ -1,4 +1,6 @@
+import { useQuery } from "@apollo/client"
 import React from "react"
+import { GET_ALL_USERS } from '../../GraphQL/queries'
 import './Single_Game.css'
 
 
@@ -7,7 +9,10 @@ const SingleGame = ({ game, setModal, userGames }) => {
   const checkIfLentOut = userGames ? 
     userGames.filter(myGame => +myGame.gameId === game.gameId).map(item => item.gameId).includes(game.gameId) && game.borrowerId
     : false
-// console.log('check if lent out: ', checkIfLentOut);
+
+  const { loading, error, data } = useQuery(GET_ALL_USERS)
+  const borrower = data && checkIfLentOut ? data.users.find(user => +user.id === +game.borrowerId).username : ''
+
   return (
     <>
       {checkIfLentOut && 
@@ -16,7 +21,9 @@ const SingleGame = ({ game, setModal, userGames }) => {
           <div className="single-borrowed-game">
             <img className="single-game-img" src={game.game.imageUrl} alt={game.game.name} />
           </div>
-          <p>borrowed by ________</p>
+          {loading && <p>loading</p>}
+          {data && <p>Borrowed by {`${borrower}`}</p>}
+          
         </div>
       }
       {!checkIfLentOut && 
