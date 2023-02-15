038a0ca40534fa76120c5c1727e703a933aa455c
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-02-14 17:00:16 -0700

    Adds a test to search results spec

--- cypress/e2e/friends-games.spec.cy.js
+++ cypress/e2e/friends-games.spec.cy.js
@@ -1,24 +1,24 @@
-// describe('Friends_Games', () => {
-//   beforeEach(() => {
-//     cy.visit('http://localhost:3000/')
-//     cy.get('.username-input')
-//       .type('randy');
-//     cy.get('.enter-site-button')
-//       .click()
-//     cy.get('.friend').eq(0).click()
-//   })
+describe('Friends_Games', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+    cy.get('.username-input')
+      .type('randy');
+    cy.get('.enter-site-button')
+      .click()
+    cy.get('.friend').eq(0).click()
+  })
 
-//   it('Should have a list of games ', () => {
-//     cy.get('.single-game-name').eq(0).should('have.text', 'Ticket to Ride')  
-//   })
+  it('Should have a list of games ', () => {
+    cy.get('.single-game-name').eq(0).should('have.text', 'Ticket to Ride')  
+  })
 
-//   it('Should have a name showing whos games the user is viewing', () => {
-//     cy.get('.friend-name').contains(`Pickafloof's Games`)
-//   })
+  it('Should have a name showing whos games the user is viewing', () => {
+    cy.get('.friend-name').contains(`Pickafloof's Games`)
+  })
 
-//   it('Should have a button that takes the useer back to there dashboard', () => {
-//     cy.get('.back-to-dash').click()
-//       .url('http://localhost:3000/dashboard')
-//   })
+  it('Should have a button that takes the user back to there dashboard', () => {
+    cy.get('.back-to-dash').click()
+      .url('http://localhost:3000/dashboard')
+  })
 
-// })
+})
