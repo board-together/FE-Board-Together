755a2ef06373f7e7bdacfe4b7c3ea85f283dbb82
Brett Kuhn <brettkuhn87@gmail.com>
2023-02-14 21:16:49 -0700

    Adds more testing and mock user data for borrowing

--- cypress/e2e/borrowing.cy.js
+++ cypress/e2e/borrowing.cy.js
@@ -5,38 +5,118 @@ const hasOperationName = (req, operationName) => {
   )
 }
 
-import { getUserDataBorrow, getAllUsersDataBorrow } from "../fixtures/fixture-borrowing"
+import { getRandyUserDataBorrow, 
+  getAllUsersDataBorrow, 
+  getHoneyUserDataBorrow, 
+  getHoneyUserDataBorrowAFTER,
+  updatedGameResponse 
+} from "../fixtures/fixture-borrowing"
 
 describe('Borrowing Functionality', () => {
-
   beforeEach(() => {
     cy.visit(`http://localhost:3000/`);
     cy.intercept('POST', 'https://board-together.herokuapp.com/graphql', (req) => {
 
       if (hasOperationName(req, 'GetUser')) {
-        req.alias = 'gqlGetUser';
+        req.alias = 'gqlGetUserQuery';
         req.reply((res) => {
-          res.body.data = getUserDataBorrow;
+          res.body.data = getRandyUserDataBorrow;
         });
       } else if (hasOperationName(req, 'GetAllUsers')) {
-        req.alias = 'gqlGetAllUsers';
+        req.alias = 'gqlGetAllUsersQuery';
         req.reply((res) => {
           res.body.data = getAllUsersDataBorrow;
         });
       }
-
-    })
-
+    });
     cy.get('.username-input')
       .type('randy');
     cy.get('.enter-site-button')
       .click()
-
   })
 
-  it('Should have a friends list', () => {
-    cy.get('h1')
-  })
+  it('Should have a place to show their borrowed games', () => {
+    cy.get('.borrowed-games-collection').should('be.empty');
+  });
+
+  it('Should be able to look at their friends\'games', () => {
+    cy.get('.friend').should('have.text', 'honey');
+    cy.intercept('POST', 'https://board-together.herokuapp.com/graphql', (req) => {
+      if (hasOperationName(req, 'GetUser')) {
+        req.alias = 'gqlGetUserQuery';
+        req.reply((res) => {
+          res.body.data = getHoneyUserDataBorrow;
+        });
+      } else if (hasOperationName(req, 'GetAllUsers')) {
+        req.alias = 'gqlGetAllUsersQuery';
+        req.reply((res) => {
+          res.body.data = getAllUsersDataBorrow;
+        });
+      }
+    })
+    cy.get('.friend').click()
+    cy.get('.friends-games-container')
+      .find('.single-game-img');
+  });
+
+  it('Should be able to click on their friends game to see a modal pop up', () => {
+    cy.intercept('POST', 'https://board-together.herokuapp.com/graphql', (req) => {
+      if (hasOperationName(req, 'GetUser')) {
+        req.alias = 'gqlGetUserQuery';
+        req.reply((res) => {
+          res.body.data = getHoneyUserDataBorrow;
+        });
+      } else if (hasOperationName(req, 'GetAllUsers')) {
+        req.alias = 'gqlGetAllUserQuery';
+        req.reply((res) => {
+          res.body.data = getAllUsersDataBorrow;
+        });
+      }
+    });
+    cy.get('.friend').click();
+    cy.get('.friends-games-container')
+      .find('.single-game-img').click();
+    cy.get('.game-modal').should('be.visible');
+    cy.contains('Village');
+  });
+
+  it('Should have a button that says "Borrow" that borrows the game', () => {
+    cy.intercept('POST', 'https://board-together.herokuapp.com/graphql', (req) => {
+      if (hasOperationName(req, 'GetUser')) {
+        req.alias = 'gqlGetUserQuery';
+        req.reply((res) => {
+          res.body.data = getHoneyUserDataBorrow;
+        });
+      } else if (hasOperationName(req, 'GetAllUsers')) {
+        req.alias = 'gqlGetAllUsersQuery';
+        req.reply((res) => {
+          res.body.data = getAllUsersDataBorrow;
+        });
+      }
+    });
+    cy.get('.friend').click();
+    cy.get('.friends-games-container')
+      .find('.single-game-img').click();
+
+    cy.intercept('POST', 'https://board-together.herokuapp.com/graphql', (req) => {
+      if (hasOperationName(req, 'GetUser')) {
+        req.alias = 'gqlGetUserQuery';
+        req.reply((res) => {
+          res.body.data = getHoneyUserDataBorrowAFTER;
+        });
+      } else if (hasOperationName(req, 'UpdateUserGame')) {
+        req.alias = 'gqlUpdateUserGameMutation';
+        req.reply((res) => {
+          res.body.data = updatedGameResponse;
+        });
+      }
+    });
+    cy.get('button').eq(1).click();
+    cy.wait(1001)
+    cy.get('.friends-games-container').should('be.empty');
+  });
+
+
 
   //refetches
   //before --- after
--- cypress/fixtures/fixture-borrowing.js
+++ cypress/fixtures/fixture-borrowing.js
@@ -1,8 +1,178 @@
-export const getUserDataBorrow = {
+export const getRandyUserDataBorrow = {
+  user: {
+    borrowedGames: [],
+    id: "2",
+    userGames: [
+      {
+        borrowerId: 9,
+        game: {
+          "__typename": "Game",
+          "id": "7",
+          "boardGameAtlasId": "hQLfeykeLS",
+          "url": "https://www.boardgameatlas.com/game/hQLfeykeLS/phase-10-card-game",
+          "name": "Phase 10",
+          "yearPublished": 1982,
+          "minPlayers": 2,
+          "maxPlayers": 6,
+          "minPlaytime": 45,
+          "maxPlaytime": 45,
+          "minAge": 8,
+          "description": "<p>Phase 10 is the rummy-type card game where players compete against one another to see who can complete 10 varied phrases first! </p>\r\n<p>A phase is a combination of cards and can be composed of sets (multiple cards of the same value), runs (multiple cards in consecutive ascending order), cards of one color, or a combination of these. Each phase to be completed is specific to each hand dealt, meaning you need to complete the current phase in order to advance to the next one! Finish all 10 and win the game!</p>\r\n<p>Featured in each deck are “Wild” and “Skip” cards that deliver game-changing moments! A “Wild” card can be used in place of a number card, as well as be used as any color to complete any phase, while a “Skip” card causes your opponent to lose a turn!</p>\r\n<p>Includes 108 cards and instructions.</p>",
+          "thumbUrl": "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1596731140183",
+          "imageUrl": "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1596731140183"
+        },
+        gameId: 7,
+        id: "693",
+        status: 0,
+        userId: 2
+      }, 
+      {
+        borrowerId: null,
+        game: {
+          "__typename": "Game",
+          "id": "23",
+          "boardGameAtlasId": "AuBvbISHR6",
+          "url": "https://www.boardgameatlas.com/game/AuBvbISHR6/ticket-to-ride",
+          "name": "Ticket to Ride",
+          "yearPublished": 2004,
+          "minPlayers": 2,
+          "maxPlayers": 5,
+          "minPlaytime": 45,
+          "maxPlaytime": 90,
+          "minAge": 8,
+          "description": "<p><strong>Ticket to Ride</strong> is a cross-country train adventure game. Players collect train cards that enable them to claim railway routes connecting cities throughout North America. The longer the routes, the more points they earn. Additional points come to those who can fulfill their Destination Tickets by connecting two distant cities, and to the player who builds the longest continuous railway. So climb aboard for some railroading fun and adventure. You've got a Ticket to Ride!<br /><br />October 2, 1900 -- it's 28 years to the day that noted London eccentric, Phileas Fogg accepted and then won a 20,000 bet that he could travel Around the World in 80 Days. Now at the dawn of the century some old friends have gathered to celebrate Fogg's impetuous and lucrative gamble -- and to propose a new wager of their own. The stakes: $1 million in a winner-takes-all competition. The objective: to see the most cities in North America -- in just 7 days. <br /><br />-Spiel Des Jahres 2004(German game of the year)<br />-As d'Or Cannes 2004 (French game of the year)<br />-Game of the year 2004 - Japan<br />-Game of the year 2004 - Sweden<br />-Game of the year 2004 - Finland<br />-Diana Jones Excellence in Gaming Award 2004<br />-Origins Award Winner - Best Board Game 2005<br />-Game of the year 2004 - Spain<br />-Parent's Choice Foundation Silver Medal 2004</p>",
+          "thumbUrl": "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324738308.jpg",
+          "imageUrl": "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324738308.jpg"
+        },
+        gameId: 23,
+        id: "702",
+        status: 0,
+        userId: 2
+      }
+    ],
+    username: "randy"
+  }
+}
+
+export const getHoneyUserDataBorrow = {
+  user: {
+    borrowedGames: [],
+    id: "2",
+    userGames: [
+      {
+        "__typename": "UserGame",
+        "id": "689",
+        "userId": 9,
+        "gameId": 18,
+        "status": 0,
+        "borrowerId": null,
+        "game": {
+            "__typename": "Game",
+            "id": "18",
+            "boardGameAtlasId": "TtPHSNukZB",
+            "url": "https://www.boardgameatlas.com/game/TtPHSNukZB/village",
+            "name": "Village",
+            "yearPublished": 2011,
+            "minPlayers": 2,
+            "maxPlayers": 4,
+            "minPlaytime": 60,
+            "maxPlaytime": 90,
+            "minAge": 12,
+            "description": "<p>In Village, each player will take the reins of a family and have them find fame and glory in many different ways. There is one thing you must not forget, however: Time will not stop for anyone and with time people will vanish. Those who will find themselves immortalized in the village chronicles will bring honor to their family and be one step closer to victory. A clever action mechanism is responsible for keeping turns short yet tactically rich and full of difficult decisions. Also unique is the way this game deals with the delicate subject of death; as a natural and perpetual part of life in the village, it keeps you focused on smart time-management.</p>",
+            "thumbUrl": "https://m.media-amazon.com/images/I/61XkXWPpGWL.jpg",
+            "imageUrl": "https://m.media-amazon.com/images/I/61XkXWPpGWL.jpg"
+        }
+      }
+    ],
+    username: "randy"
+  }
+}
+
+export const getHoneyUserDataBorrowAFTER = {
   user: {
     borrowedGames: [],
     id: "2",
     userGames: [],
+    username: "randy"
+  }
+}
+
+export const getRandyUserDataBorrowAFTER ={
+  user: {
+    borrowedGames: [],
+    id: "2",
+    userGames: [
+      {
+        borrowerId: 9,
+        game: {
+          "__typename": "Game",
+          "id": "7",
+          "boardGameAtlasId": "hQLfeykeLS",
+          "url": "https://www.boardgameatlas.com/game/hQLfeykeLS/phase-10-card-game",
+          "name": "Phase 10",
+          "yearPublished": 1982,
+          "minPlayers": 2,
+          "maxPlayers": 6,
+          "minPlaytime": 45,
+          "maxPlaytime": 45,
+          "minAge": 8,
+          "description": "<p>Phase 10 is the rummy-type card game where players compete against one another to see who can complete 10 varied phrases first! </p>\r\n<p>A phase is a combination of cards and can be composed of sets (multiple cards of the same value), runs (multiple cards in consecutive ascending order), cards of one color, or a combination of these. Each phase to be completed is specific to each hand dealt, meaning you need to complete the current phase in order to advance to the next one! Finish all 10 and win the game!</p>\r\n<p>Featured in each deck are “Wild” and “Skip” cards that deliver game-changing moments! A “Wild” card can be used in place of a number card, as well as be used as any color to complete any phase, while a “Skip” card causes your opponent to lose a turn!</p>\r\n<p>Includes 108 cards and instructions.</p>",
+          "thumbUrl": "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1596731140183",
+          "imageUrl": "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1596731140183"
+        },
+        gameId: 7,
+        id: "693",
+        status: 0,
+        userId: 2
+      }, 
+      {
+        borrowerId: null,
+        game: {
+          "__typename": "Game",
+          "id": "23",
+          "boardGameAtlasId": "AuBvbISHR6",
+          "url": "https://www.boardgameatlas.com/game/AuBvbISHR6/ticket-to-ride",
+          "name": "Ticket to Ride",
+          "yearPublished": 2004,
+          "minPlayers": 2,
+          "maxPlayers": 5,
+          "minPlaytime": 45,
+          "maxPlaytime": 90,
+          "minAge": 8,
+          "description": "<p><strong>Ticket to Ride</strong> is a cross-country train adventure game. Players collect train cards that enable them to claim railway routes connecting cities throughout North America. The longer the routes, the more points they earn. Additional points come to those who can fulfill their Destination Tickets by connecting two distant cities, and to the player who builds the longest continuous railway. So climb aboard for some railroading fun and adventure. You've got a Ticket to Ride!<br /><br />October 2, 1900 -- it's 28 years to the day that noted London eccentric, Phileas Fogg accepted and then won a 20,000 bet that he could travel Around the World in 80 Days. Now at the dawn of the century some old friends have gathered to celebrate Fogg's impetuous and lucrative gamble -- and to propose a new wager of their own. The stakes: $1 million in a winner-takes-all competition. The objective: to see the most cities in North America -- in just 7 days. <br /><br />-Spiel Des Jahres 2004(German game of the year)<br />-As d'Or Cannes 2004 (French game of the year)<br />-Game of the year 2004 - Japan<br />-Game of the year 2004 - Sweden<br />-Game of the year 2004 - Finland<br />-Diana Jones Excellence in Gaming Award 2004<br />-Origins Award Winner - Best Board Game 2005<br />-Game of the year 2004 - Spain<br />-Parent's Choice Foundation Silver Medal 2004</p>",
+          "thumbUrl": "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324738308.jpg",
+          "imageUrl": "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1629324738308.jpg"
+        },
+        gameId: 23,
+        id: "702",
+        status: 0,
+        userId: 2
+      },
+      {
+        "__typename": "UserGame",
+        "id": "689",
+        "userId": 9,
+        "gameId": 18,
+        "status": 0,
+        "borrowerId": null,
+        "game": {
+            "__typename": "Game",
+            "id": "18",
+            "boardGameAtlasId": "TtPHSNukZB",
+            "url": "https://www.boardgameatlas.com/game/TtPHSNukZB/village",
+            "name": "Village",
+            "yearPublished": 2011,
+            "minPlayers": 2,
+            "maxPlayers": 4,
+            "minPlaytime": 60,
+            "maxPlaytime": 90,
+            "minAge": 12,
+            "description": "<p>In Village, each player will take the reins of a family and have them find fame and glory in many different ways. There is one thing you must not forget, however: Time will not stop for anyone and with time people will vanish. Those who will find themselves immortalized in the village chronicles will bring honor to their family and be one step closer to victory. A clever action mechanism is responsible for keeping turns short yet tactically rich and full of difficult decisions. Also unique is the way this game deals with the delicate subject of death; as a natural and perpetual part of life in the village, it keeps you focused on smart time-management.</p>",
+            "thumbUrl": "https://m.media-amazon.com/images/I/61XkXWPpGWL.jpg",
+            "imageUrl": "https://m.media-amazon.com/images/I/61XkXWPpGWL.jpg"
+        }
+      }
+    ],
     username: "randy"
   }
 }
@@ -18,4 +188,17 @@ export const getAllUsersDataBorrow = {
       username: "randy"
     }
   ]
+}
+
+export const updatedGameResponse = {
+    "updateUserGame": {
+        "userGame": {
+            "id": "702",
+            "borrowerId": 2,
+            "status": 1,
+            "__typename": "UserGame"
+        },
+        "errors": [],
+        "__typename": "UpdateUserGamePayload"
+    }
 }
