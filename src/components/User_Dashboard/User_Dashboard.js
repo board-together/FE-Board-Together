aee45f1e0d6ff8500bc88c34c61e16aad93ddce5
Spencer Haka <spencer.haka@gmail.com>
2023-02-14 21:18:15 -0700

    Add hover effect for buttons and friends

--- src/components/User_Dashboard/User_Dashboard.js
+++ src/components/User_Dashboard/User_Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ReactSVG } from "react-svg";
 import { Link } from 'react-router-dom'
 import { Navbar } from '../Navbar/Navbar'
 import { GameModal } from '../Game_Modal/Game_Modal'
@@ -6,6 +7,7 @@ import SingleGame from '../Single_Game/Single_Game'
 import './User_Dashboard.css'
 import { GET_ALL_USERS } from '../../GraphQL/queries'
 import { useQuery } from '@apollo/client'
+import smile from '../../assets/face-smile-regular.svg'
 
 
 
@@ -15,10 +17,10 @@ export const UserDashboard = ({ userInfo, searchBarSubmit, deleteGame, setModal,
   const allUsersError = useQuery(GET_ALL_USERS).error
   const allUsersData = useQuery(GET_ALL_USERS).data
   const friendsList = allUsersData && userInfo ? allUsersData.users.filter(user => user.username !== userInfo.username) : []
-  const smile = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm177.6 62.1C192.8 334.5 218.8 352 256 352s63.2-17.5 78.4-33.9c9-9.7 24.2-10.4 33.9-1.4s10.4 24.2 1.4 33.9c-22 23.8-60 49.4-113.6 49.4s-91.7-25.5-113.6-49.4c-9-9.7-8.4-24.9 1.4-33.9s24.9-8.4 33.9 1.4zM144.4 208a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm192-32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
+  const smileImg = <img src={smile} alt='smile svg' className='smile-svg'/>
   const friends = friendsList.map(friend => {
     return (<Link to={`/friends-games/${friend.username}`} key={friend.id}>
-      <p key={friend.id} className="friend">{smile}{friend.username}</p>
+      <span key={friend.id} className="friend">{smileImg}<p className='friend-name'>{friend.username}</p></span>
     </Link>)
   })
 
