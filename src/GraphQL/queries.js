7d396eed4e77a72a4a6db88829f0a6a86568b2c9
Brett Kuhn <brettkuhn87@gmail.com>
2023-02-14 19:44:19 -0700

    Adds operation names to queries

--- src/GraphQL/queries.js
+++ src/GraphQL/queries.js
@@ -7,7 +7,7 @@ import { gql } from "@apollo/client"
 //Get All Users (Friends)
 
 export const GET_USER = (userName) => gql`
-   query {
+   query GetUser {
     user(username: "${userName}") {
       id
       username
@@ -107,7 +107,7 @@ export const GET_MODAL_GAME = (name) => gql`
   `
 
 export const GET_ALL_USERS = gql`
-    query {
+    query GetAllUsers {
       users {
           id
           username
