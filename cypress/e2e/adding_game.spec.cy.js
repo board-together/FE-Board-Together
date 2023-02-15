4c293b323c7e50901ed897c8f9954662f8225da8
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-02-15 11:06:53 -0700

    Adds intercepts to specand tests that use it

--- cypress/e2e/adding_game.spec.cy.js
+++ cypress/e2e/adding_game.spec.cy.js
@@ -19,7 +19,7 @@ describe('Search_Results', () => {
       .click()
     cy.intercept('POST', "https://board-together.herokuapp.com/graphql", (req) => {
       cy.stub()
-        .callsFake(req => req.reply({ fixture: 'single_user.json' })).as('user')
+        .callsFake(req => req.reply({ fixture: 'search_games.json' })).as('searchResults')
     })
     cy.get(':nth-child(1) > .game-name-Catan').contains('Catan')
       .click()
@@ -31,20 +31,20 @@ describe('Search_Results', () => {
       .should('include', 'http://localhost:3000/dashboard')
   })
 
-  // it('When game is added it should apear in game collection', () => {
-  //   cy.get('.search-input')
-  //     .type('Catan')
-  //   cy.get('.search-button')
-  //     .click()
-  //   cy.get(':nth-child(1) > .game-name-Catan').contains('Catan')
-  //     .click()
-  //   cy.get('.modal-button')
-  //     .click()
-  //   cy.get('[href="/dashboard/"] > button')
-  //     .click()
-  //   cy.url()
-  //     .should('include', 'http://localhost:3000/dashboard')
+  it('When game is added it should apear in game collection', () => {
+    cy.get('.search-input')
+      .type('Catan')
+    cy.get('.search-button')
+      .click()
+    cy.get(':nth-child(1) > .game-name-Catan').contains('Catan')
+      .click()
+    cy.get('.modal-button')
+      .click()
+    cy.get('[href="/dashboard/"] > button')
+      .click()
+    cy.url()
+      .should('include', 'http://localhost:3000/dashboard')
 
-  // })
+  })
 
 })
