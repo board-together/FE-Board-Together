162f62b57a231029f7abb8fc45fda89e2f8a5b5a
Spencer Haka <spencer.haka@gmail.com>
2023-04-11 10:56:38 -0600

    Add confirmation for user account deletion

--- src/components/DeleteUser/DeleteUser.js
+++ src/components/DeleteUser/DeleteUser.js
@@ -5,11 +5,13 @@ import user from "../../assets/user-solid.svg"
 
 function DeleteUser() {
 
-  const [toggle, setToggle] = useState(false)
-
   const handleClick = (e) => {
-    document.getElementById('delete-user-container').classList.toggle('hidden')
-    e.target.classList.toggle('user-options-icon-focus')
+    if (e) {
+      document.getElementById('delete-user-container').classList.toggle('hidden')
+      e.target.classList.toggle('user-options-icon-focus')
+    } else {
+      document.getElementById('confirmation-container').classList.toggle('hidden')
+    }
   }
 
   return (
@@ -17,8 +19,12 @@ function DeleteUser() {
       <img src={user} alt='user options' className='user-options-icon' id='user-options-icon' onClick={(event) => handleClick(event)} />
       <div className='delete-user-container hidden' id='delete-user-container'>
         <div className='delete-user'>
-          Delete My Account
           <Link to={'/'} className='link-to-login'>Log Out</Link>
+          <button className='delete-user-button' id='delete-user-button' onClick={handleClick}>Delete My Account</button>
+          <div className='confirmation-container hidden' id='confirmation-container'>
+            <p style={{ 'fontSize': '14px', 'fontStyle': 'italic' }}>Please type your username below to confirm account deletion</p>
+            <input type='text' className='confirmation'></input>
+          </div>
         </div>
       </div>
     </>
