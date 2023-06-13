7dc6ccca6d38ae030936742f8fe2a363df950551
Spencer Haka <spencer.haka@gmail.com>
2023-06-13 10:20:57 -0600

    Finish including delete functionality

--- src/components/DeleteUser/DeleteUser.js
+++ src/components/DeleteUser/DeleteUser.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import "./DeleteUser.css"
 import { Link, useNavigate } from 'react-router-dom'
 import { useLazyQuery, useMutation } from '@apollo/client'
@@ -8,18 +8,36 @@ import user from "../../assets/user-solid.svg"
 
 function DeleteUser() {
 
+  const navigate = useNavigate()
   const [toggle, setToggle] = useState(true)
   const [confirmUsername, setConfirmUsername] = useState('')
   const [userId, setUserId] = useState(null)
   const [errorMessage, setErrorMessage] = useState(false)
-  const [getUserId, { data, error }] = useLazyQuery(GET_USER_ID, { variables: { username: confirmUsername } })
+  const [getUserId, { data }] = useLazyQuery(GET_USER_ID, { variables: { username: confirmUsername } })
   const deleteUserMutation = useMutation(DELETE_USER)
   const deleteUserFunc = deleteUserMutation[0]
-  const deleteUserResponse = deleteUserMutation[1]
+  const deleteUserResponse = deleteUserMutation[1].data
+
+  const getTheUserId = useCallback(() => { getUserId() }, [getUserId])
 
   useEffect(() => {
-    deleteUserFunc({ variables: { input: userId } })
-  }, [userId])
+    if (userId) {
+      const doIT = async () => {
+        await deleteUserFunc({ variables: { input: { id: userId } } })
+      }
+      doIT()
+    }
+  }, [userId, deleteUserFunc, navigate, deleteUserResponse])
+
+  useEffect(() => {
+    if (deleteUserResponse) {
+      navigate('../')
+    }
+  }, [deleteUserResponse, navigate])
+
+  useEffect(() => {
+    getTheUserId()
+  }, [confirmUsername, getTheUserId])
 
   const handleClick = (e) => {
     let target = e.target.id
@@ -32,10 +50,9 @@ function DeleteUser() {
     }
   }
 
-  const handleDelete = async () => {
-    await getUserId()
+  const handleDelete = () => {
     if (data) {
-      setUserId(data)
+      setUserId(data.user.id)
     } else {
       setErrorMessage(true)
     }
@@ -43,7 +60,8 @@ function DeleteUser() {
 
   const confirmation = <div className='confirmation-container' id='confirmation-container'>
     <p style={{ 'fontSize': '14px', 'fontStyle': 'italic' }}>Please type your username below to confirm account deletion</p>
-    <input type='text' className='confirmation' onChange={(event) => setConfirmUsername(event.target.value)}></input>
+    <input type='text' className='confirmation' onChange={(event) => setConfirmUsername(event.target.value)}>
+    </input>
     <div className='confirmation-buttons'>
       <button onClick={handleDelete}>Ok, Delete It</button>
       <button onClick={() => setToggle(true)}>Nevermind</button>
